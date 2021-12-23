cd70d39dbef9fbd6bbe78afe9825d1c6cb4498e5
NirelShunem <nirel2007@gmail.com>
2021-12-23 19:05:48 +0200

    added localStorage support for new todo, fixed style for form

--- src/app/components/new-todo/new-todo.component.ts
+++ src/app/components/new-todo/new-todo.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { Form } from '@angular/forms';
+import { Form, NgForm } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { ITodo } from 'src/app/models/ITodo';
+import { TodoService } from 'src/app/services/todo.service';
+import { v4 as uuidv4 } from 'uuid';
+
 
 
 @Component({
@@ -10,16 +14,34 @@ import { ITodo } from 'src/app/models/ITodo';
 })
 export class NewTodoComponent implements OnInit {
 
-  @ViewChild('f') form:Form
+  @ViewChild('f') form:NgForm;
 
-  constructor() { }
+  constructor( private todoService:TodoService ,public dialog:MatDialog) { }
 
   ngOnInit(): void {
   }
 
   public onNewTodoSubmit():void{
-    console.log(this.form)
-    console.log("newSubmit")
+if(this.form.valid){
+  const formValue=this.form.form.value;
+    const newTodo:ITodo={
+      id:uuidv4(),
+      title:formValue.title,
+      description:formValue.description,
+      endDate:formValue.date,
+      isCompleted:false,
+      isArchived:false,
+      selected:false,
+    }
+    this.todoService.addNewTodo(newTodo);
+    this.dialog.closeAll();
+}
+
+  
+    
+
+  
+
   }
 
 
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, Subject, Subscription } from 'rxjs';
+import { BehaviorSubject, Observable} from 'rxjs';
 import { ITodo } from '../models/ITodo';
 
 @Injectable({
@@ -7,115 +7,15 @@ import { ITodo } from '../models/ITodo';
 })
 export class TodoService {
 
-  private mock:ITodo[]=[
-    {
-      "id":1,
-      "title":"Pacific gull",
-      "description":"Gabianus pacificus",
-      "isCompleted":false,
-      "isArchived":false,
-      "endDate":"2/21/2021",
-      "selected":true
-  },
-  {
-    "id":2,
-    "title":"Sea birds (unidentified)",
-    "description":"unavailable",
-    "isCompleted":false,
-    "isArchived":false,
-    "endDate":"3/28/2021",
-    "selected":false
-  },
-  {
-    "id":3,
-    "title":"Beaver, american",
-    "description":"Castor canadensis",
-    "isCompleted":false,
-    "isArchived":false,
-    "endDate":"2/23/2021",
-    "selected":false
-  },
-  {
-    "id":4,
-    "title":"Black rhinoceros",
-    "description":"Diceros bicornis",
-    "isCompleted":false,
-    "isArchived":false,
-    "endDate":"2/24/2021",
-    "selected":false
-  },
-  {
-    "id":5,
-    "title":"Squirrel, antelope ground"
-    ,"description":"Ammospermophilus nelsoni"
-    ,"isCompleted":false,
-    "isArchived":false,
-    "endDate":"10/30/2021",
-    "selected":false
-  },
-  {
-    "id":6,
-    "title":"Pie, indian tree",
-    "description":"Dendrocitta vagabunda",
-    "isCompleted":false,
-    "isArchived":false,
-    "endDate":"4/30/2021",
-    "selected":false
-  },
-  {
-    "id":7,
-    "title":"Blesbok",
-    "description":"Damaliscus dorcas",
-    "isCompleted":false,
-    "isArchived":false,
-    "endDate":"7/27/2021",
-    "selected":false
-  },
-  {
-    "id":8,
-    "title":"Red-tailed wambenger",
-    "description":"Phascogale calura",
-    "isCompleted":false,
-    "isArchived":false,
-    "endDate":"9/11/2021",
-    "selected":false
-  },
-  {
-    "id":9,
-    "title":"Grey heron",
-    "description":"Ardea cinerea",
-    "isCompleted":false,
-    "isArchived":false,
-    "endDate":"6/26/2021",
-    "selected":false
-  },
-  {
-    "id":10,
-    "title":"Barrows goldeneye",
-    "description":"Bucephala clangula",
-    "isCompleted":false,
-    "isArchived":false,
-    "endDate":"4/9/2021",
-    "selected":false
-  },
-  {
-    "id":11,
-    "title":"Boa, emerald green tree",
-    "description":"Boa caninus",
-    "isCompleted":false,
-    "isArchived":false,
-    "endDate":"4/11/2021",
-    "selected":false
-  },
-  ]
+ private todos: Array<ITodo>=[]
 
-  private _todoSubject:BehaviorSubject<Array<ITodo>>=new BehaviorSubject(this.mock);
+  private _todoSubject:BehaviorSubject<Array<ITodo>>=new BehaviorSubject(this.todos);
 
-  
+
+
+  private _singleTodoSubject:BehaviorSubject<ITodo>=new BehaviorSubject(this.todos.length?this.todos[0]:null)
 
   constructor() { }
-
-  private _singleTodoSubject:BehaviorSubject<ITodo>=new BehaviorSubject(this.mock[0])
 
   public getSelectedTodo():Observable<ITodo>{
     return this._singleTodoSubject.asObservable();
@@ -123,10 +23,27 @@ export class TodoService {
   }
 
   public getTodo():Observable<Array<ITodo>>{
+    if(!this._todoSubject.value.length){
+      const todosString=localStorage.getItem("todos");
+      if(todosString){
+        const existingTodos:Array<ITodo>=JSON.parse( todosString);
+        existingTodos[0].selected=true;
+        this._todoSubject.next(existingTodos);
+        this._singleTodoSubject.next(existingTodos[0]);
+      }
+    }
     return this._todoSubject.asObservable();
   }
 
   public setSelectedTodo(todo:ITodo){
     this._singleTodoSubject.next(todo);
+  }
+
+
+  public addNewTodo(newTodo:ITodo):void{
+    const existingTodo :Array<ITodo>=this._todoSubject.value;
+    existingTodo.push(newTodo);
+    this._todoSubject.next(existingTodo);
+    localStorage.setItem("todos",JSON.stringify(existingTodo));
   }
 }
